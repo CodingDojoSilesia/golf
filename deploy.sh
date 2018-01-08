dfdb638a15369f36135f1f54dfd536f8499be834
Marek Piechula <m.piechula@clearcode.cc>
2018-01-08 19:04:25 +0100

    AGAIN fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 docker build . -t cc_golf_app
-docker stop cc_golf_db cc_golf_app
-docker run --name cc_golf_db --env-file .env -d postgres
-docker run --rm -it --env-file .env --link db cc_golf_app python3 install.py
+docker stop cc_golf_db cc_golf_app_uwsgi
 docker run \
-    --rm -it \
-    --env-file .env \
+    --name cc_golf_db \
+    --env-file .env -d \
+    --restart unless-stopped \
+    postgres
+docker run \
+    --name cc_golf_app_install \
+    --rm -it --env-file .env \
+    --link cc_golf_db \
+    cc_golf_app python3 install.py
+docker run \
+    --name cc_golf_app_uwsgi \
+    --rm -dit --env-file .env \
     -p 80:5000 \
     --tmpfs /code \
     --memory 256m \
