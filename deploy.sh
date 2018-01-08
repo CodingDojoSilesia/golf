9060dd680aff0d2b78bf1f9fa9b1c473e327f8b9
Marek Piechula <m.piechula@clearcode.cc>
2018-01-08 19:00:35 +0100

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ docker stop cc_golf_db cc_golf_app
 docker run --name cc_golf_db --env-file .env -d postgres
 docker run --rm -it --env-file .env --link db cc_golf_app python3 install.py
 docker run \
-    --name cc_golf_app \
     --rm -it \
     --env-file .env \
     -p 80:5000 \
