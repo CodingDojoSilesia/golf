0eb0cfbefc6b9afa52988eb4f9dd84016502d72f
Firemark <marpiechula@gmail.com>
2018-12-16 13:45:10 +0100

    dont update hero when old score is equal or less than new score

--- code/app.py
+++ code/app.py
@@ -160,17 +160,23 @@ def execute_order_66():
 
 def submit_score(nick, lang, code, seconds=0.0):
     hero = Hero.query.filter_by(nick=nick).first()
-    score = len(code)
+    new_score = len(code)
     if hero is None:
         old_score = '-'
-        hero = Hero(nick, lang, score)
+        hero = Hero(nick, lang, new_score)
     else:
         old_score = hero.score
-        hero.score = min(old_score, score)
+        if old_score <= new_score:
+            logger.warning(
+                'Worse Record[%r, %s] in %0.2f seconds, from %s to %s',
+                nick, lang, seconds, old_score, new_score,
+            )
+            return
+        hero.score = new_score
 
     logger.info(
         'New Record[%r, %s] in %0.2f seconds, from %s to %s',
-        nick, lang, seconds, old_score, score,
+        nick, lang, seconds, old_score, new_score,
     )
 
     db.session.add(hero)
