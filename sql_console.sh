1be05ea4e5eeeb775072e12d8a6af52adc42befa
Firemark <marpiechula@gmail.com>
2018-12-16 14:39:10 +0100

    fix sql console script

--- sql_console.sh
+++ sql_console.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 set -e
-
+source .env
 docker run \
-    --name cc_golf_db \
-    --env-file .env -d \
-    --volume "$(pwd)/pgdata":/var/lib/postgresql/data \
     -it --rm \
-    --link cc_golf_db:postgres
-    postgres psql 
+    --link cc_golf_db:db \
+	-e PGHOST=db \
+	-e PGDATABASE=$POSTGRES_DB \
+	-e PGUSER=$POSTGRES_USER \
+	-e PGPASSWORD=$POSTGRES_PASSWORD \
+    postgres psql
